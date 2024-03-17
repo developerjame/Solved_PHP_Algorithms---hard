c8a23ff8a1c621810f1a7a8d817653bf6c16a335
developerjame <119099522+developerjame@users.noreply.github.com>
2024-03-17 23:06:44 +0300

    solution

---
+++ First Missing Positive/solution.php
@@ -0,0 +1,36 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function firstMissingPositive($nums) {
+        $n = count($nums);
+
+        for($i=0;$i<$n;$i++){
+            if($nums[$i]<=0){
+                $nums[$i] = $n+1;
+            }
+        }
+
+        for($i=0;$i<$n;$i++){
+            $num = abs($nums[$i]);
+            if($num < $n){
+                $nums[$num-1] = -abs($nums[$num-1]);
+            }
+        }
+
+        for($i=0;$i<$n;$i++){
+            if($nums[$i] > 0){
+                return $i+1;
+            }
+        }
+        return $n;
+    }
+}
+$nums = [1,-1,2,3,5];
+
+$solution = new Solution();
+echo $solution->firstMissingPositive($nums);
+?>
